e1da90a70561c9a13eb15a9c00487af91d0a17dc
Sauni Weerasinghe <it21373848@my.sliit.lk>
2023-07-03 14:43:11 +0530

    modified home url

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -65,7 +65,7 @@ class Navbar extends Component {
           </button>
           <div className="collapse navbar-collapse" id="navbar1">
             <ul className="nav-tabs1"> 
-              <Link to="/#" className="nav-link">
+              <Link to="/" className="nav-link">
               <img src={process.env.PUBLIC_URL + "/img/appLogo.png"} className="app-logo" alt="home" /> 
                </Link> 
             </ul>
